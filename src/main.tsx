83e05e6f8780f5ca0e88b6dfae15a4f2b9f3287f
SahutoL <96646448+SahutoL@users.noreply.github.com>
2024-07-06 08:59:53 +0900

    main.tsx を更新

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,15 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
-import OneSignal from "react-onesignal";
+//import OneSignal from "react-onesignal";
 
+/*
 OneSignal.init({
   appId: import.meta.env.VITE_PUBLIC_ONESIGNAL_APP_ID || "",
 });
 
 OneSignal.showSlidedownPrompt();
+*/
 
 ReactDOM.render(
   <React.StrictMode>
